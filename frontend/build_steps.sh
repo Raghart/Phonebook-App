9da12aea086a8ace5f5a20b8f1cb528f3db9b3bd
Rafael Rojas Diaz <118632412+Raghart@users.noreply.github.com>
2025-03-15 17:24:14 -0400

    Building code

---
+++ frontend/build_steps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Build script"
+
+# Trigger deployment
+#curl https://api.render.com/deploy/srv-${{ secrets.RENDER_SERVICE_ID }}?key=${{ secrets.RENDER_API_KEY }}
+
+# Install dependencies
+npm install
+
+# Install Build
+npm install build
